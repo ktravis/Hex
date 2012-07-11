a17bb8c811cba5189125a148919171bef7113c40
Kyle Travis (Laptop) <wetscience@gmail.com>
2012-07-11 10:44:57 -0700

    Working hex grid generation, including split tiles.

--- src/com/Grid.java
+++ src/com/Grid.java
@@ -22,28 +22,127 @@ public class Grid {
 	}
 	
 	void fill() {
-		int rowSize = 6;
-		
-		
 		if (tiles == null) return;
-
-		for (int index = 0; index < MAX_TILES; index++) { 
-			for (int row = 0; row < MAX_ROWS; row++) {
+		
+		int rowSize = 6;
+		int index = 0;
+		
+		Tile.Type startType = Tile.Type.CENTER;
+		Tile.Type endType = Tile.Type.CENTER;
+		
+		for (int row = 0; row < MAX_ROWS; row++) {
+			if (row == 8 || row == 11 || row ==	27 || row == 30) {
+				startType = endType = Tile.Type.CENTER;
 				
+				switch (row) {
+				case 8: rowSize = 28; break;
+				case 11: rowSize = 31; break;
+				case 27: rowSize = 31; break;
+				case 30: rowSize = 28; break;
+				}
+			} else if (row < 11 && row > 8) {
+				startType = Tile.Type.UR_CORNER;
+				endType = Tile.Type.UL_CORNER;
+				rowSize = 28 + (row - 8);
+			} else if (row < 8) {
+				startType = Tile.Type.UR_EDGE;
+				endType = Tile.Type.UL_EDGE;
+				rowSize = 6 + row*3;
+			} else if (row < 27 && row > 11) {
+				if (row % 2 == 0) {
+					startType = Tile.Type.SPLIT_LEFT;
+					endType = Tile.Type.SPLIT_RIGHT;
+					rowSize = 32;
+				} else {
+					startType = endType = Tile.Type.CENTER;
+					rowSize = 31;
+				}
+				
+				if (row > 14 && row < 24) {
+					switch (row) {
+					case 15: rowSize = 32; break;
+					case 23: rowSize = 32; break;
+					case 16: rowSize = 38; break;
+					case 22: rowSize = 38; break;
+					case 17: rowSize = 38; break;
+					case 19: rowSize = 38; break;
+					case 21: rowSize = 38; break;
+					case 18: rowSize = 40; break;
+					case 20: rowSize = 40; break;
+					}
+				}
+				
+			} else if (row < 30 && row > 27) {
+				startType = Tile.Type.LR_CORNER;
+				endType = Tile.Type.LL_CORNER;
+				rowSize = 31 - (row - 27);
+			} else if (row > 30) {
+				startType = Tile.Type.LR_EDGE;
+				endType = Tile.Type.LL_EDGE;
+				rowSize = 27 - (row - 31)*3;
 			}
+			
+			
+			System.out.print(row);
+			System.out.print(" ");
+			System.out.print(index);
+			System.out.print(" ");
+			for (int i = 0; i < rowSize; i++) {
+				
+				Tile.Type t = Tile.Type.CENTER;
+
+				if (i == 0) {
+					t = startType;
+				} else if (i == rowSize - 1) {
+					t = endType;
+				}
+				
+				if (row == 15 || row == 23) {
+					if (i == 15) t = Tile.Type.SPLIT_RIGHT;
+					else if (i == 16) t = Tile.Type.SPLIT_LEFT;
+				} else if (row == 16 || row == 22) {
+					if (i > 12 && i < 25) {
+						if (i % 2 == 0) t = Tile.Type.SPLIT_LEFT;
+						else t = Tile.Type.SPLIT_RIGHT;
+					}
+				} else if (row == 17 || row == 19 || row == 21) {
+					if (i > 11 && i < 26) {
+						if (i % 2 == 0) t = Tile.Type.SPLIT_RIGHT;
+						else t = Tile.Type.SPLIT_LEFT;
+					}
+				} else if (row == 18 || row == 20) {
+					if (i > 11 && i < 28) {
+						if (i % 2 == 0) t = Tile.Type.SPLIT_RIGHT;
+						else t = Tile.Type.SPLIT_LEFT;
+					}
+				}
+				
+				if (index == 2 || index == 3) t = Tile.Type.TOP_CORNER;
+				if (index == 1020 || index == 1021) t = Tile.Type.BOTTOM_CORNER;
+				
+				Tile newTile = new Tile(this, t);
+				newTile.setRow(row);
+				
+				index += 1;
+				
+				
+				System.out.print(t);
+			}
+			System.out.print("\n");
 		}
 		
 	}
 	
-	void add() {
+	void add(Tile t) {
 		if (tiles == null || tiles.size() == MAX_TILES) return;
-		tiles.add(new Tile(this, tiles.size()));
+		tiles.add(t);
 	}
 	
 	Tile getTile(int i) {
 		return tiles.get(i);
 	}
 	
+	//WRONG, fix
 	int[] getNeighbors(int i) {
 		int[] n = new int[6];
 		
--- src/com/Tile.java
+++ src/com/Tile.java
@@ -1,27 +1,32 @@
 package com;
 
+//NOTE : If tileType == SPLIT -> is 
+
+
+
 public class Tile {
 	private float x, y;
 	private int index;
 	private Grid parent;
 	private Type tileType;
+	private int row = 0;
 
 	// enum "Type" describes the placement of the tile on the grid
 	public enum Type {
-		CENTER, TOP_CORNER, UL_EDGE, URT_EDGE, 
+		CENTER, TOP_CORNER, UL_EDGE, UR_EDGE, 
 		UL_CORNER, UR_CORNER, L_EDGE, R_EDGE, LL_CORNER, LR_CORNER, 
-		LL_EDGE, LR_EDGE, BOTTOM_CORNER, SPLIT
+		LL_EDGE, LR_EDGE, BOTTOM_CORNER, SPLIT_LEFT, SPLIT_RIGHT //split left/right refers to REMAINING half of tile, relative to initial tile numbering (right to left, top to bottom)
 	}
 
-	public Tile(Grid parent) {
+	public Tile(Grid parent, Type t) {
 		this.parent = parent;
-		tileType = Type.CENTER;
+		tileType = t;
 	}
 
-	public Tile(Grid parent, int i) {
+	public Tile(Grid parent, Type t, int i) {
 		this.parent = parent;
 		index = i;
-		tileType = Type.CENTER;
+		tileType = t;
 	}
 
 	void draw() {
@@ -46,4 +51,7 @@ public class Tile {
 	void remove() {
 		parent.remove(this);
 	}
+	
+	void setRow(int r) { row = r; }
+	int getRow() { return row; }
 }
